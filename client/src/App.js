cd3f7f8860f946c6bf9b4a50d655e02988689347
Amine Benzaazaa <Medzaza910@gmail.com>
2023-02-23 21:29:10 +0100

    CreatePost

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,21 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
-  const [listsofPosts, setListsofPosts] = useState([]);
-
-  useEffect(()=>{
-    axios.get("http://localhost:3001/posts").then((response)=>{
-      setListsofPosts(response.data)
-      console.log(listsofPosts);
-    })
-  },[])
-  
-
   return (
     <div className="App">
-      {
-        listsofPosts.map((value,key)=> { 
-          return <div className='post'>
-            <div className='title'>
-              {value.title}
-            </div>
-            <div className='body'>
-              {value.postText}
-            </div>
-            <div className='footer'>
-              {value.username}
-            </div>
-            </div>
-        })
-      }
+      <Router>
+        <div className="navbar">
+          <Link Link to="/createpost"> Create A Post</Link>
+          <Link to="/"> Home Page</Link>
+        </div>
+        <Routes>
+          <Route path="/"  element={<Home/>} />
+          <Route path="/createpost"  element={<CreatePost/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ client/src/pages/CreatePost.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import axios from 'axios';
+import {Formik, Form, Field, ErrorMessage} from 'formik'
+import * as Yup from 'yup'
+
+function CreatePost() {
+    const initialValues = {
+        title: "",
+        postText: "",
+        username:""
+    } 
+    const validationSchema = Yup.object().shape({
+        title: Yup.string().required("You must fill this input"),
+        postText: Yup.string().required("You must fill this input"),
+        username: Yup.string().min(3).max(15).required("You must fill this input"),
+    })
+    const onSubmit = (data) =>{
+        axios.post("http://localhost:3001/posts", data).then((response)=>{
+            console.log("Data inserted successfully")
+    })
+    }
+  return (
+    <div>
+        <Formik initialValues={initialValues} 
+                onSubmit={onSubmit} 
+                validationSchema={validationSchema}
+            >
+            <Form className='formContainer'>
+                <label htmlFor="title">Title</label>
+                <Field  id="inputCreatePost" name="title" placeholder="(ex. Title...)"/>
+                <ErrorMessage name="title" component="span"/>
+                <label htmlFor="title">postText</label>
+                <Field id="inputCreatePost" name="postText" placeholder="(ex. Post Text...)"/>
+                <ErrorMessage name="postText" component="span"/>
+                <label htmlFor="title">username</label>
+                <Field id="inputCreatePost" name="username" placeholder="(ex. Username...)"/>
+                <ErrorMessage name="username" component="span"/>
+                <button type='submit'>Create a Post</button>
+            </Form>
+        </Formik>
+    </div>
+  )
+}
+
+export default CreatePost
